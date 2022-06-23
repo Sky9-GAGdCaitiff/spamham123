f94891fa00b58afd9802f5b68ca442053571f2f3
Sky9-GAGdCaitiff <97184805+Sky9-GAGdCaitiff@users.noreply.github.com>
2022-06-23 12:55:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import joblib
 #load the joblib model 
 model_nb = joblib.load('spam-ham')
 
+
 #user input 
 st.title("SPAM HAM CLASSIFICATION")
 ip = st.text_input("Enter your message:")
+
 
 #predict if the entered message is spam or ham 
 op = model_nb.predict([ip])
